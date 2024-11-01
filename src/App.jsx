69e3189a6ff896370731d62ecbdec08c9ebd56e7
Amir Razvi <amirrazvi8@gmail.com>
2024-11-01 09:47:56 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ function App() {
                     <div className="relative w-full h-0.5">
                         <button
                             type="button"
-                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-red-500 text-white px-2 py-0.5"
                             onClick={swap}
                         >
                             swap
@@ -69,7 +69,7 @@ function App() {
                             amountDisable
                         />
                     </div>
-                    <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                    <button type="submit" className="w-full bg-red-500 text-white px-4 py-3 rounded-lg">
                         Convert {from.toUpperCase()} to {to.toUpperCase()}
                     </button>
                 </form>
